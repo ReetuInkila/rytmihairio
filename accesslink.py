d35579548195e48309d6534d635c999a5f0c62cd
lrtinkiv <lrtinkiv@student.jyu.fi>
2023-04-17 08:59:21 +0300

    perus accesslink metodeja luotu

--- accesslink.py
+++ accesslink.py
@@ -1,5 +1,5 @@
 
-from utils import load_config
+from utils import load_config, save_config
 import requests
 
 # Config tiedostossa on tallennettuna client id ja client secret sekä muita autorisointi tokeneita
@@ -15,5 +15,29 @@ def getUserInfo():
         return(r.json())
     else:
         return(r)
+    
+def createTransaction():
+    headers = {'Accept': 'application/json',  'Authorization': 'Bearer '+config['access_token']}
 
-print(getUserInfo()) 
+    r = requests.post('https://www.polaraccesslink.com/v3/users/'+str(config['user_id'])+'/exercise-transactions', headers = headers)
+
+    if r.status_code >= 200 and r.status_code < 400 and r.status_code != 204:
+        response = r.json()
+        config["transaction_id"] = response['transaction-id']
+        save_config(config, CONFIG_FILENAME)
+    else:
+        return r
+    
+def listExercises():
+
+    headers = {'Accept': 'application/json',  'Authorization': 'Bearer '+config['access_token']}
+    r = requests.get('https://www.polaraccesslink.com/v3/users/'+str(config['user_id'])+'/exercise-transactions/'+str(config['transaction_id']), headers = headers)
+
+    if r.status_code >= 200 and r.status_code < 400:
+        return(r.json())
+    else:
+        return(r)
+
+
+print(createTransaction())
+print(listExercises())
