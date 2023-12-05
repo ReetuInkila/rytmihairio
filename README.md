6323acaed3d050d3c8b543cb318e6bfd62eed450
lrtinkiv <lrtinkiv@jyu.fi>
2023-12-05 14:54:47 +0200

    Web UI started

--- README.md
+++ README.md
@@ -1,4 +1,11 @@
 # syke
+
+## Current state of development
+- The web user interface for viewing my training data can be found at
+    https://reetuinkila-401304.ew.r.appspot.com
+- Login to page using 'kirjaudu' as Salasana.
+
+
 ## Tools for identifying arrhythmias from heart rate data.
 My goal is to develop a tool with which I can easily monitor heart rate data produced by a sports watch. 
 The program's functionalities are/will be:
--- app/main.py
+++ app/main.py
@@ -1,16 +1,46 @@
 
-from flask import Flask
-
+from functools import wraps
+from flask import Flask, redirect, render_template, request, session, url_for
 
 # Entrypoint
 app = Flask(__name__)
 
+app.config.from_pyfile('config.py')
+
+# Käyttäjän autentikoimiseen vaadittavat päätepisteet
+def login_required(f):
+    @wraps(f)
+    def decorated_function(*args, **kwargs):
+        if 'user' not in session:
+            return redirect(url_for('login'))
+        return f(*args, **kwargs)
+    return decorated_function
+
+@app.route('/logout')
+def logout():
+    session.clear()
+    return redirect('/login')
+
+@app.route("/login", methods=['GET', 'POST'])
+def login():
+    error = None
+
+    if request.method == 'POST':
+        password = request.form['password']
+
+        # Tarkistetaan onko salasana oikea. Oikeasti salasana tulisi enkoodata, mutta tässä tapauksessa sen vuotaminen ei ole merkityksellistä
+        if password == 'kirjaudu':
+            session['user'] = 'user'
+            return redirect(url_for('home'))
+        else:
+            error = 'Väärä salasana'
+
+    return render_template('login.xhtml', error = error)
 
 @app.route("/")
-def hello():
-
-    return "Sivu toimii!"
-
+@login_required
+def home():
+    return render_template('index.xhtml')
 
 if __name__ == "__main__":
     # This is used when running locally only. When deploying to Google App
