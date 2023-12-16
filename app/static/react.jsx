1b406879bbc89d613eff8ba5b55a4536d5dc1bfa
lrtinkiv <lrtinkiv@jyu.fi>
2023-12-16 19:15:02 +0200

    all data from fit

--- app/static/react.jsx
+++ app/static/react.jsx
@@ -1,66 +1,83 @@
 const App = () => {
+    const [hrData, setHrData] = React.useState([]);
+    const [gpxData, setGpxData] = React.useState([]);
 
-    // Kun sivu avataan luodaan kartta
+    // Kun sivu avataan haetaan treenin data
     React.useEffect(() => {
-        // Your Leaflet initialization and map creation here
-        hae_gpx(id)
-        .then(function(gpx) {
-            var map = L.map('map').setView(gpx[0], 13);
+        hae_treeni_data(id)
+        .then((data) => {
+            let times = data.map((entry) => entry.timestamp);
+            let hr = data.map((entry) => entry.heart_rate);
+            setHrData({times:times, hr:hr});
 
+            let gpx = data.map((entry) => [entry.lat, entry.lon]);
+            let start = gpx[0];
+            let stop = gpx[gpx.length-1];
+
+            for(let i= 0; i<gpx.length; i++){
+                if(etaisyys(start, gpx[i])<500){
+                    gpx.splice(i, 1);
+                    i--;
+                }else {
+                    break;
+                }
+
+            }
+            for(let i = gpx.length-1; i>-1; i--){
+                if(etaisyys(stop, gpx[i])<500){
+                    gpx.splice(i, 1);
+                }else {
+                    break;
+                }
+            }
+            console.log(gpx);
+            var map = L.map('map').setView(gpx[0], 13);
             L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
                 attribution: '© OpenStreetMap contributors'
             }).addTo(map);
             
             let polyline = L.polyline(gpx, {color: 'red'}).addTo(map);
-        })
+        });
+
     }, []);
 
 
     return (
         <div>
-            <HrPlotter/>
+            <HrPlotter
+                hrData={hrData}
+            />
         </div>
         
     );
 };
 
-const HrPlotter = () => {
-    const [fitData, setFitData] = React.useState([]);
-  
+const HrPlotter = function(props) {
     React.useEffect(() => {
-        hae_hr_data(id)
-        .then((data) => setFitData(data));
-    }, []);
-  
-    React.useEffect(() => {
-      if (fitData.length > 0) {
-
-        let xValues = fitData.map((entry) => entry.timestamp);
-        let yValues = fitData.map((entry) => entry.heart_rate);
-
-        let ctx = document.getElementById('hrChart').getContext('2d');
-        new Chart(ctx, {
-            type: "line",
-            data: {
-              labels: xValues,
-              datasets: [{
-                label: 'hr',
-                fill: false,
-                lineTension: 0,
-                backgroundColor: "rgba(0,0,255,1.0)",
-                borderColor: "rgba(0,0,255,0.1)",
-                data: yValues
-              }]
-            },
-            options: {
-              legend: {display: false},
-              scales: {
-                yAxes: [{ticks: {min: 6, max:16}}],
-              }
-            }
-          });
-      }
-    }, [fitData]);
+        if (props.hrData.hr) {
+            let ctx = document.getElementById('hrChart').getContext('2d');
+            new Chart(ctx, {
+                type: "line",
+                data: {
+                labels: props.hrData.times,
+                datasets: [{
+                    label: 'hr',
+                    fill: false,
+                    lineTension: 0,
+                    backgroundColor: "rgba(0,0,255,1.0)",
+                    borderColor: "rgba(0,0,255,0.1)",
+                    data: props.hrData.hr
+                }]
+                },
+                options: {
+                legend: {display: false},
+                scales: {
+                    yAxes: [{ticks: {min: 6, max:16}}],
+                }
+                }
+            });
+        }
+    }, [props.hrData]);
   
     return (
         <canvas id="hrChart" ></canvas>
@@ -70,49 +87,8 @@ const HrPlotter = () => {
 
 let baseUrl = window.location.href;
 
-async function hae_gpx(id){
-    let url = new URL(baseUrl+"/gpx/"+id);
-    let response = await fetch(url);
-    if (!response.ok) {
-        throw new Error(`HTTP error! status: ${response.status}`);
-    }
-    let data = await response.text();
-    let parser = new DOMParser();
-    let xmlDoc = parser.parseFromString(data, 'text/xml');
-    let trkptElements= xmlDoc.getElementsByTagName('trkpt');
-    // Convert HTMLCollection to an array of arrays
-    let coordinatesArray = Array.from(trkptElements).map(trkpt => {
-        return [
-            parseFloat(trkpt.getAttribute('lat')),
-            parseFloat(trkpt.getAttribute('lon'))
-        ];
-    });
-
-    let start = coordinatesArray[0];
-    let stop = coordinatesArray[coordinatesArray.length-1];
-
-    for(let i= 0; i<coordinatesArray.length; i++){
-        if(etaisyys(start, coordinatesArray[i])<500){
-            coordinatesArray.splice(i, 1);
-            i--;
-        }else {
-            break;
-        }
-
-    }
-    for(let i = coordinatesArray.length-1; i>-1; i--){
-        if(etaisyys(stop, coordinatesArray[i])<500){
-            coordinatesArray.splice(i, 1);
-        }else {
-            break;
-        }
-    }
-
-    return coordinatesArray;
-}
-
-async function hae_hr_data(id){
-    let url = new URL(baseUrl+"/hr/"+id);
+async function hae_treeni_data(id){
+    let url = new URL(baseUrl+"/data/"+id);
     let response = await fetch(url);
     if (!response.ok) {
         throw new Error(`HTTP error! status: ${response.status}`);
