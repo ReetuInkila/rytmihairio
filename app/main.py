8416485ce9559f5908cc6a856d21ed51532459dd
lrtinkiv <lrtinkiv@jyu.fi>
2023-12-28 10:39:21 +0200

    recaptcha to work locally

--- app/main.py
+++ app/main.py
@@ -82,5 +82,5 @@ def data(id):
         response.headers['Content-Type'] = 'text/plain'
     return response
 
-if __name__ == "__main__":
-    app.run(host='0.0.0.0', port=5000)
+if __name__ == '__main__':
+    app.run(debug=True)
