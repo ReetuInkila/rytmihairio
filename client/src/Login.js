abb0a2840649b0e2f7751f473abb82da1d7f8c79
lrtinkiv <lrtinkiv@jyu.fi>
2023-12-30 07:46:24 +0200

    added login page

---
+++ client/src/Login.js
@@ -0,0 +1,31 @@
+import React, { useState } from 'react';
+
+const Login = ({ onLogin }) => {
+  const [username, setUsername] = useState('');
+  const [password, setPassword] = useState('');
+
+  const handleLogin = () => {
+    // Add authentication logic here (e.g., API call, etc.)
+    // For simplicity, let's assume the login is successful
+    onLogin(username);
+  };
+
+  return (
+    <div>
+      <h2>Login Page</h2>
+      <label>
+        Username:
+        <input type="text" value={username} onChange={(e) => setUsername(e.target.value)} />
+      </label>
+      <br />
+      <label>
+        Password:
+        <input type="password" value={password} onChange={(e) => setPassword(e.target.value)} />
+      </label>
+      <br />
+      <button onClick={handleLogin}>Login</button>
+    </div>
+  );
+};
+
+export default Login;
