09e6faf88d0dd86fb911a9b9a0b5e9249e459a03
lrtinkiv <lrtinkiv@jyu.fi>
2024-01-05 14:20:21 +0200

    JsDoc

--- web-app/src/main/Map.js
+++ web-app/src/main/Map.js
@@ -2,16 +2,21 @@ import React from 'react';
 import { MapContainer, TileLayer, Polyline } from 'react-leaflet';
 import 'leaflet/dist/leaflet.css';
 
-function Map(props) {
+/**
+ * Component to return react leaflet map where route is drawn
+ * @param {[number, number][]} gpx list of gpx pairs stored in lists
+ * @returns Leaflet map
+ */
+function Map({gpx}) {
   return (
     <div style={{ height: '100%', width: '100%' }}>
-      {props.gpx.length > 0 && (
-        <MapContainer center={props.gpx[0]} zoom={14} style={{ height: '100%', width: '100%' }}>
+      {gpx.length > 0 && (
+        <MapContainer center={gpx[0]} zoom={14} style={{ height: '100%', width: '100%' }}>
           <TileLayer
             url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
             attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
           />
-          <Polyline positions={props.gpx} color="blue" weight={5} />
+          <Polyline positions={gpx} color="blue" weight={5} />
         </MapContainer>
       )}
     </div>
