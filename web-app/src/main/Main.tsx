8db6c14040f62c321c812b7214bcb0e525d6b979
lrtinkiv <lrtinkiv@jyu.fi>
2024-01-05 08:41:28 +0200

    Main to typescript

--- web-app/src/main/Main.tsx
+++ web-app/src/main/Main.tsx
@@ -6,10 +6,19 @@ import Map from './Map'
 
 
 function Main() {
-    const [hrData, setHrData] = useState([]);
+    const [hrData, setHrData] = useState<{ times: string[], hr: number[] }>({ times: [], hr: [] });
     const [gpxData, setGpxData] = useState([]);
     const [distance, setDistance] = useState(0);
     const [time, setTime] = useState("00:00:00");
+
+    // Define an interface for the structure of the objects in data.timestamps
+    interface TimestampEntry {
+        timestamp: string;
+        heart_rate: number;
+        lat: number;
+        lon: number;
+    }
+  
 
     const url = 'https://syke-backend-w6xkb6ulza-lz.a.run.app/data';
   
@@ -24,13 +33,13 @@ function Main() {
         }).then((res) =>
             res.json().then((data) => {
                 console.log(data);
-                let times = data.timestamps.map((entry) => entry.timestamp);
-                let hr = data.timestamps.map((entry) => entry.heart_rate);
-                setHrData({times:times, hr:hr});
+                let times = data.timestamps.map((entry:TimestampEntry) => entry.timestamp);
+                let hr = data.timestamps.map((entry:TimestampEntry) => entry.heart_rate);
+                setHrData({times, hr});
 
                 let gpx = data.timestamps
-                    .filter((entry) => 'lat' in entry && 'lon' in entry)
-                    .map((entry) => [entry.lat, entry.lon]);
+                    .filter((entry:TimestampEntry) => 'lat' in entry && 'lon' in entry)
+                    .map((entry:TimestampEntry) => [entry.lat, entry.lon]);
                 setGpxData(gpx);
 
                 setDistance((data.distance/1000));
