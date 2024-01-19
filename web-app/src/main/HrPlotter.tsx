547bc409bdcf1809a43156186db31bd30a23f5fd
lrtinkiv <lrtinkiv@jyu.fi>
2024-01-19 10:15:42 +0200

    uses react chart js 2 to hr and elevation chart

--- web-app/src/main/HrPlotter.tsx
+++ web-app/src/main/HrPlotter.tsx
@@ -1,106 +1,111 @@
-import React, { useEffect } from 'react';
-import Chart from "chart.js/auto";
+import React, { useEffect, useState } from 'react';
+import {
+    Chart as ChartJS,
+    CategoryScale,
+    LinearScale,
+    PointElement,
+    LineElement,
+    Title,
+    Tooltip,
+    Legend,
+    ChartData,
+  } from 'chart.js';
+import { Line } from 'react-chartjs-2';
+
+ChartJS.register(
+    CategoryScale,
+    LinearScale,
+    PointElement,
+    LineElement,
+    Title,
+    Tooltip,
+    Legend
+  );
 
 type hrPlotterProps = {
     hrData: {times:Array<string>, hr:Array<number>};
     alt:Array<number>;
 };
 
-function HrPlotter({hrData, alt}:hrPlotterProps) {
-    useEffect(() => {
-        let chartInstance:Chart;
+function HrPlotter({ hrData, alt }: hrPlotterProps) {
+    const [hr, setHr] = useState([40, 220]);
+    const [data, setData] = useState<ChartData<'line', ChartData<'line'>['datasets'][0]['data'], unknown>>({
+      datasets: [],
+      labels: [],
+    });
 
-        if (hrData.hr) {
-            // Find min and max heart rate values
-            let minHr = Math.min(...hrData.hr);
-            let maxHr = Math.max(...hrData.hr);
+    function calculateColor(value:number){
+        let red = 255;
+        let green = 255;
+        let midHr = hr[0] + (hr[1] - hr[0]) / 2;
 
-            // Function to calculate color based on heart rate value
-            const calculateColor = (value:number) => {
-                let red = 255;
-                let green = 255;
-                let midHr = minHr + (maxHr - minHr) / 2;
-
-                if (value > midHr) {
-                    // Calculate green based on the range between minHr and avgHr
-                    const normalized = 1 - (value - midHr) / (maxHr - midHr);
-                    green = Math.round(255 * normalized);
-                } else if (value < midHr) {
-                    // Calculate red based on the range between avgHr and maxHr
-                    const normalized = (value - minHr) / (midHr - minHr);
-                    red = Math.round(255 * normalized);
-                }
-                return `rgba(${red}, ${green}, 0, 1)`;
-            };
-
-            // Destroy existing chart if it exists
-            let existingCanvas = document.getElementById('hrChart') as HTMLCanvasElement | null;
-            if (existingCanvas) {
-                Chart.getChart(existingCanvas)?.destroy();
-                
-                // Create a new chart
-                let ctx = existingCanvas.getContext('2d');
-                if(ctx){
-                    chartInstance = new Chart(ctx, {
-                        type: "line",
-                        data: {
-                            labels: hrData.times,
-                            datasets: [{
-                                label: 'HR',
-                                fill: false,
-                                showLine: false,
-                                backgroundColor: hrData.hr.map(calculateColor),
-                                borderColor: hrData.hr.map(calculateColor),
-                                pointRadius: 2,
-                                data: hrData.hr,
-                                yAxisID: 'hr',
-                            },{
-                                label: 'Elevation',
-                                fill: true,
-                                showLine: true,
-                                pointRadius: 0,
-                                data: alt,
-                                pointStyle: false,
-                                yAxisID: 'alt',
-                            }]
-                        },
-                        options: {
-                            plugins: {
-                                legend: {
-                                    display: false
-                                },
-                            },
-                            scales: {
-                                hr: {
-                                    beginAtZero: false,
-                                    min: minHr,
-                                    max: maxHr,
-                                },
-                                alt: {
-                                    beginAtZero: false,
-                                    position: 'right',
-                                    grid: {
-                                        drawOnChartArea: false,
-                                    }
-                                }
-                            }
-                        }
-                    });
-                }
-                
-            }
+        if (value > midHr) {
+            // Calculate green based on the range between minHr and avgHr
+            const normalized = 1 - (value - midHr) / (hr[1] - midHr);
+            green = Math.round(255 * normalized);
+        } else if (value < midHr) {
+            // Calculate red based on the range between avgHr and maxHr
+            const normalized = (value - hr[0]) / (midHr - hr[0]);
+            red = Math.round(255 * normalized);
         }
+        return `rgba(${red}, ${green}, 0, 1)`;
+    };
 
-        // Cleanup function to destroy the chart on component unmount or when props.hrData changes
-        return () => {
-            if (chartInstance) {
-                chartInstance.destroy();
-            }
-        };
+    useEffect(() => {
+        let minHr = Math.min(...hrData.hr);
+        let maxHr = Math.max(...hrData.hr);
+        setHr([minHr, maxHr]);
+
+        setData({
+            labels: hrData.times,
+            datasets: [
+              {
+                label: 'HR',
+                data: hrData.hr,
+                fill: false,
+                backgroundColor: hrData.hr.map(calculateColor),
+                borderColor: hrData.hr.map(calculateColor),
+                yAxisID: 'hr',
+              },{
+                label: 'Elevation',
+                fill: true,
+                showLine: true,
+                pointRadius: 0,
+                data: alt,
+                yAxisID: 'alt',
+            }]
+        });
     }, [hrData, alt]);
 
+    const options = {
+        responsive: true,
+        interaction: {
+          mode: 'index' as const,
+          intersect: false,
+        },
+        stacked: false,
+        scales: {
+          hr: {
+            type: 'linear' as const,
+            display: true,
+            position: 'left' as const,
+          },
+          alt: {
+            type: 'linear' as const,
+            display: true,
+            position: 'right' as const,
+            grid: {
+              drawOnChartArea: false,
+            },
+          },
+        },
+    };
+
+    
+
+
     return (
-        <canvas id="hrChart"></canvas>
+        <Line id="hrChart" data={data} options={options}/>
     );
 };
 
