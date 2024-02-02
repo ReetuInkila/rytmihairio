c9fb461437d214f7a1289e26ca93e5842ae7ed27
lrtinkiv <lrtinkiv@jyu.fi>
2024-02-02 11:30:49 +0200

    new service account

--- web-app/src/main/HrPlotter.tsx
+++ web-app/src/main/HrPlotter.tsx
@@ -11,6 +11,7 @@ import {
 import { Line } from 'react-chartjs-2';
 import zoom from 'chartjs-plugin-zoom';
 
+
 ChartJS.register(
     CategoryScale,
     LinearScale,
@@ -26,33 +27,32 @@ type hrPlotterProps = {
 };
 
 function HrPlotter({ hrData, alt }: hrPlotterProps) {
-    const [hr, setHr] = useState([40, 220]);
     const [data, setData] = useState<ChartData<'line', ChartData<'line'>['datasets'][0]['data'], unknown>>({
       datasets: [],
       labels: [],
     });
 
-    function calculateColor(value:number){
-        let red = 255;
-        let green = 255;
-        let midHr = hr[0] + (hr[1] - hr[0]) / 2;
-
-        if (value > midHr) {
-            // Calculate green based on the range between minHr and avgHr
-            const normalized = 1 - (value - midHr) / (hr[1] - midHr);
-            green = Math.round(255 * normalized);
-        } else if (value < midHr) {
-            // Calculate red based on the range between avgHr and maxHr
-            const normalized = (value - hr[0]) / (midHr - hr[0]);
-            red = Math.round(255 * normalized);
-        }
-        return `rgba(${red}, ${green}, 0, 1)`;
-    };
-
     useEffect(() => {
         let minHr = Math.min(...hrData.hr);
         let maxHr = Math.max(...hrData.hr);
-        setHr([minHr, maxHr]);
+
+        // Function to calculate color based on heart rate value
+        const calculateColor = (value:number) => {
+          let red = 255;
+          let green = 255;
+          let midHr = minHr + (maxHr - minHr) / 2;
+
+          if (value > midHr) {
+              // Calculate green based on the range between minHr and avgHr
+              const normalized = 1 - (value - midHr) / (maxHr - midHr);
+              green = Math.round(255 * normalized);
+          } else if (value < midHr) {
+              // Calculate red based on the range between avgHr and maxHr
+              const normalized = (value - minHr) / (midHr -minHr);
+              red = Math.round(255 * normalized);
+          }
+          return `rgba(${red}, ${green}, 0, 1)`;
+        };
 
         setData({
             labels: hrData.times,
@@ -100,13 +100,10 @@ function HrPlotter({ hrData, alt }: hrPlotterProps) {
                 enabled: true
               },
               mode: 'x' as const,
-            }
+            },
           }
-        },
+        }
     };
-
-    
-
 
     return (
         <Line id="hrChart" data={data} options={options}/>
